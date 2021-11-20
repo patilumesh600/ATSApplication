c0ee6a2a3b4fefa483f94cd8bd4a80fd9da94668
sagar1prajapati <sagarprajapati2010@gmail.com>
2021-11-20 17:19:30 +0530

    Sp call get all asset and delete asset call

---
+++ ATSApplication/Interfaces/IAssetService.cs
@@ -0,0 +1,16 @@
+﻿using ATSApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATSApplication.Interfaces
+{
+    public interface IAssetService
+    {
+        List<Asset_VM> GetAll();
+        string Delete(int ID);
+        string AddOrUpdate(Asset_VM Asset);
+    }
+}
